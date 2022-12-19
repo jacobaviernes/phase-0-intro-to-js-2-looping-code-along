e3e8abb10cdcf838f7aae5375c7a4e8508ec1b90
Jacob Viernes <jacobaviernes@gmail.com>
2022-12-19 12:11:19 -0800

    commit tree 7cc2da205358ed68e2e7e7f70d8415431ede8eaf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards( names, event ) {
+    let completedCards= []
+    for(let i = 0; i < names.length; i++){
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards);
+    }
+    return completedCards;
+}
+
+writeCards(names, "Surprise")
+
+let down=[11];
+
+function countDown(down){
+    debugger;
+    while(down >= 0 ){
+        console.log(down--);
+    }
+    return down;
+}
+
+countDown(down);
